0f0f8a4ef8400226d6017c29491d07386a7ef526
byDsh116 <by.dsh116@gmail.com>
2024-02-01 11:24:00 +0300

    add router

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const indexRouter = require('./routers/indexRouter');
+
+const { PORT } = process.env.PORT;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(process.cwd(), 'public')));
+app.use(express.json());
+app.use(cors());
+
+app.use('/', indexRouter);
+
+app.listen(PORT, () => {
+  console.log(`SERVER STARTED ON PORT===> ${PORT}`);
+});
