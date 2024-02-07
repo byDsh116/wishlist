b672ec88014c5185ed7b6df83fc6e1be4c879d38
byDsh116 <by.dsh116@gmail.com>
2024-02-07 11:58:38 +0300

    add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import RegistrationForm from './components/RegistrationForm';
+import HomePage from './pages/HomePage';
 
 function App(): JSX.Element {
   return (
     <>
       <div> just test 0.1</div>
-      <RegistrationForm />
+      <Routes>
+        <Route path='/reg' element={<RegistrationForm />} />
+        <Route path='/home' element={<HomePage />} />
+      </Routes>
     </>
   );
 }
