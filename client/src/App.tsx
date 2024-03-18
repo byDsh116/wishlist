db2e56915b8402f977781da03ad645024475988b
byDsh116 <by.dsh116@gmail.com>
2024-03-18 11:57:44 +0300

    rename reg form, create file for login and registration button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import RegistrationForm from './components/RegistrationForm';
 import HomePage from './pages/HomePage';
 import UserPage from './pages/UserPage';
 import Navbar from './components/Navbar';
 import Cookies from 'js-cookie';
+import RegistrationPage from './components/RegistrationPage';
 
 function App(): JSX.Element {
   const cookie = Cookies.get('Dsh');
+
   return (
     <>
       <Navbar cookie={cookie || ''} />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/user/:username' element={<UserPage />} />
-        <Route path='/registration' element={<RegistrationForm />} />
+        <Route path='/registration' element={<RegistrationPage />} />
       </Routes>
     </>
   );
