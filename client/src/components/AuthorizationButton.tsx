e6222d2cb02f8159cadac324dc6e95d61dc6a5e6
byDsh116 <by.dsh116@gmail.com>
2024-04-30 11:46:25 +0300

    add handelClik redirect for reg/log button

--- client/src/components/AuthorizationButton.tsx
+++ client/src/components/AuthorizationButton.tsx
@@ -1,22 +1,34 @@
 import { useReducer } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { AuthActionTypes } from '../types/types';
 import { authReducer } from '../redux/authReducer';
 
 export default function AuthorizationButton() {
   const location = useLocation();
-  const [state, dispatch] = useReducer(authReducer, { isLoggedIn: false });
+  // const [state, dispatch] = useReducer(authReducer, { isLoggedIn: false });
+  // const handleClick = () => {
+  //   dispatch({ type: AuthActionTypes.LOGOUT });
+  // };
+  const navigate = useNavigate();
+
   const handleClick = () => {
-    dispatch({ type: AuthActionTypes.LOGOUT });
+    if (location.pathname === '/') {
+      navigate('registration');
+    } else {
+      navigate('/');
+    }
   };
 
   return (
     <button onClick={handleClick}>
-      {location.pathname === '/'
-        ? 'Registration'
-        : state.isLoggedIn
-        ? 'Logout'
-        : 'Login'}
+      {location.pathname === '/' ? 'Registration' : 'Login'}
     </button>
+    // <button onClick={handleClick}>
+    //   {location.pathname === '/'
+    //     ? 'Registration'
+    //     : state.isLoggedIn
+    //     ? 'Logout'
+    //     : 'Login'}
+    // </button>
   );
 }
