f47fa4c9b72effe56715fafbf9004c17ff8801a6
byDsh116 <by.dsh116@gmail.com>
2024-05-01 12:26:38 +0300

    hidden button if cookie

--- client/src/components/AuthorizationButton.tsx
+++ client/src/components/AuthorizationButton.tsx
@@ -2,33 +2,30 @@ import { useReducer } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { AuthActionTypes } from '../types/types';
 import { authReducer } from '../redux/authReducer';
+import Cookies from 'js-cookie';
 
 export default function AuthorizationButton() {
+  const cookie = Cookies.get('Dsh');
   const location = useLocation();
-  // const [state, dispatch] = useReducer(authReducer, { isLoggedIn: false });
-  // const handleClick = () => {
-  //   dispatch({ type: AuthActionTypes.LOGOUT });
-  // };
+  const [state, dispatch] = useReducer(authReducer, { isLoggedIn: false });
+
   const navigate = useNavigate();
 
   const handleClick = () => {
     if (location.pathname === '/') {
       navigate('registration');
+      if (state.isLoggedIn) {
+        // Cookies.remove('Dsh', { path: '/' });
+        dispatch({ type: AuthActionTypes.LOGIN });
+      }
     } else {
       navigate('/');
     }
   };
 
   return (
-    <button onClick={handleClick}>
+    <button onClick={handleClick} className={cookie ? 'hidden' : ''}>
       {location.pathname === '/' ? 'Registration' : 'Login'}
     </button>
-    // <button onClick={handleClick}>
-    //   {location.pathname === '/'
-    //     ? 'Registration'
-    //     : state.isLoggedIn
-    //     ? 'Logout'
-    //     : 'Login'}
-    // </button>
   );
 }
