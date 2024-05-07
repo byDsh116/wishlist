782c2a326f07f87e7bc81a88e29f30534531d374
byDsh116 <by.dsh116@gmail.com>
2024-05-07 17:49:51 +0300

    disable import for modal

--- client/src/pages/UserPage.tsx
+++ client/src/pages/UserPage.tsx
@@ -1,6 +1,6 @@
 import Cookies from 'js-cookie';
 import HomePage from './HomePage';
-import CreateRoomModal from '../components/CreateRoomModal';
+// import CreateRoomModal from '../components/CreateRoomModal';
 
 export default function UserPage() {
   const cookie = Cookies.get('Dsh');
